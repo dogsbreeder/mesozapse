65eb6d62d175516aa1a45c69ff822d74013bdd56
dogsbreeder <xxzeljkoxx@gmail.com>
2025-03-14 00:12:48 +0100

    [Cursor] Add metadataBase configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
+  images: {
+    unoptimized: true,
+  },
+  metadata: {
+    metadataBase: new URL('https://mesozapse.pages.dev'),
+  },
 }
 
 module.exports = nextConfig 
